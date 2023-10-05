1cc5b51aae57d686345d8318121ecb16bb7495d0
root <root@LAP-JCHOI.QCIConsulting.com>
2023-10-05 14:00:21 -0500

    finally worked

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,16 @@ module.exports = {
   entry: './app/main.ts',
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "src/dist/bundle.js"
+    filename: "dist/bundle.js"
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as a resolvable extension.
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
   }
 }
